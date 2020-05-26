af330613893605d53ae197b651f115e892a5e738
sen.n <wwsenbonkusahaeruww@gmail.com>
2020-05-26 11:14:18 +0000

    signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,17 +4,38 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
+  
 
   def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = '登録完了しました。'
+      redirect_to @user
+    else
+      flash[:danger] = '登録に失敗しました。'
+      render :new
+    end
   end
+  
 
   def edit
   end
 
   def update
   end
-end
+
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile)
+  end
+  
+end  
